cd57629e467acad68a22b789a88327ecab66bcdd
parash <parash.shresh@gmail.com>
2022-02-22 18:19:54 +0545

    Larabel

---
+++ photogram/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/',function(){
+    return view('home');
+});
+Route::get('/slide',function(){
+    return view('slide');
+});
+
+Route::get('/home','PostsController@index');
+
+Auth::routes();
+
+Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
+Route::get('/p/create','PostsController@create');
+Route::get('/p/{post}','PostsController@show');
+
+Route::post('/p','PostsController@store');
+Route::get('/search','SearchController@search')->name('search');
